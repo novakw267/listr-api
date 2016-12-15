9ef46947c505a7d939aafe14fb91ab31fa6e715f
racheltstevens <racheltstevens@gmail.com>
2016-12-15 09:59:33 -0500

    Modify scripts to use port 4741 and variables
    Update from rails-api-template
    Close #3

--- scripts/create-example.sh
+++ scripts/create-example.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
 API="http://localhost:4741"
-URL_PATH="/sign-up"
+URL_PATH="/examples"
 curl "${API}${URL_PATH}" \
   --include \
   --request POST \
   --header "Content-Type: application/json" \
+  --header "Authorization: Token token=$TOKEN" \
   --data '{
-    "credentials": {
-      "email": "'"${EMAIL}"'",
-      "password": "'"${PASSWORD}"'",
-      "password_confirmation": "'"${PASSWORD}"'"
+    "example": {
+      "text": "'"${TEXT}"'"
     }
   }'
 
